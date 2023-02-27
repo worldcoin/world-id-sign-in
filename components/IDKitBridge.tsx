a82bd73c583c7ac4b49a9143acfb602361828443
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-02-27 14:29:12 -0800

    this hurts my soul

--- components/IDKitBridge.tsx
+++ components/IDKitBridge.tsx
@@ -3,94 +3,94 @@ import { useEffect } from "react";
 import { Spinner } from "./Spinner";
 
 interface IIDKitBridge {
-	client_id: string;
-	nonce: string;
-	toggleHeader: (visible: boolean) => void;
-	onSuccess: (result: ISuccessResult) => void;
+  client_id: string;
+  nonce: string;
+  toggleHeader: (visible: boolean) => void;
+  onSuccess: (result: ISuccessResult) => void;
 }
 
 export const IDKitBridge = ({
-	client_id,
-	nonce,
-	toggleHeader,
-	onSuccess,
+  client_id,
+  nonce,
+  toggleHeader,
+  onSuccess,
 }: IIDKitBridge): JSX.Element => {
-	const { reset, qrData, result, errorCode, verificationState } =
-		IDKitInternal.useAppConnection(
-			client_id,
-			"",
-			nonce,
-			"Sign in with World ID",
-			"4e15bfc7b9842886c4e49d8f8ef04cf1"
-		);
+  const { reset, qrData, result, errorCode, verificationState } =
+    IDKitInternal.useAppConnection(
+      client_id,
+      "",
+      nonce,
+      "Sign in with World ID",
+      "4e15bfc7b9842886c4e49d8f8ef04cf1"
+    );
 
-	useEffect(() => {
-		console.log("result", result);
-	}, [result]);
+  useEffect(() => {
+    console.log("result", result);
+  }, [result]);
 
-	useEffect(() => {
-		console.log("errorCode", errorCode);
-	}, [errorCode]);
+  useEffect(() => {
+    console.log("errorCode", errorCode);
+  }, [errorCode]);
 
-	useEffect(() => {
-		console.log("verificationState", verificationState);
+  useEffect(() => {
+    console.log("verificationState", verificationState);
 
-		if (
-			verificationState === IDKitInternal.VerificationState.AwaitingConnection
-		) {
-			toggleHeader(true);
-		} else {
-			toggleHeader(false);
-		}
+    if (
+      verificationState === IDKitInternal.VerificationState.AwaitingConnection
+    ) {
+      toggleHeader(true);
+    } else {
+      toggleHeader(false);
+    }
 
-		if (verificationState === IDKitInternal.VerificationState.Failed) {
-			reset();
-		}
+    if (verificationState === IDKitInternal.VerificationState.Failed) {
+      reset();
+    }
 
-		if (
-			verificationState === IDKitInternal.VerificationState.Confirmed &&
-			result
-		) {
-			onSuccess(result);
-		}
-	}, [verificationState, reset, toggleHeader, onSuccess, result]);
+    if (
+      verificationState === IDKitInternal.VerificationState.Confirmed &&
+      result
+    ) {
+      onSuccess(result);
+    }
+  }, [verificationState, reset, toggleHeader, onSuccess, result]);
 
-	return (
-		<div className="mt-8">
-			{verificationState ===
-				IDKitInternal.VerificationState.AwaitingConnection && (
-				<>
-					{!qrData?.default && <Spinner />}
-					{qrData?.default && (
-						<IDKitInternal.QRCode
-							data={qrData?.default}
-							logoSize={0}
-							size={250}
-						/>
-					)}
-				</>
-			)}
-			{verificationState !==
-				IDKitInternal.VerificationState.AwaitingConnection && (
-				<>
-					<Spinner />
-					{verificationState ===
-						IDKitInternal.VerificationState.AwaitingVerification && (
-						<>
-							<h1 className="font-medium text-xl mt-6">Confirm in World App</h1>
-							<div className="text-text-muted mt-2">
-								Waiting for your response
-							</div>
-						</>
-					)}
-				</>
-			)}
-		</div>
-	);
+  return (
+    <div className="mt-8">
+      {verificationState ===
+        IDKitInternal.VerificationState.AwaitingConnection && (
+        <>
+          {!qrData?.default && <Spinner />}
+          {qrData?.default && (
+            <IDKitInternal.QRCode
+              data={qrData?.default}
+              logoSize={0}
+              size={250}
+            />
+          )}
+        </>
+      )}
+      {verificationState !==
+        IDKitInternal.VerificationState.AwaitingConnection && (
+        <>
+          <Spinner />
+          {verificationState ===
+            IDKitInternal.VerificationState.AwaitingVerification && (
+            <>
+              <h1 className="font-medium text-xl mt-6">Confirm in World App</h1>
+              <div className="text-text-muted mt-2">
+                Waiting for your response
+              </div>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 {
-	/* <IDKitWidget
+  /* <IDKitWidget
 app_id={params.client_id}
 action=""
 signal={params.nonce}
--- pages/login.tsx
+++ pages/login.tsx
@@ -8,89 +8,89 @@ import { Spinner } from "@/components/Spinner";
 import { IDKitBridge } from "@/components/IDKitBridge";
 
 export default function Login() {
-	const router = useRouter();
-	const [params, setParams] = useState<IAuthorizeRequest>();
-	const [headerShown, setHeaderShown] = useState(true);
+  const router = useRouter();
+  const [params, setParams] = useState<IAuthorizeRequest>();
+  const [headerShown, setHeaderShown] = useState(true);
 
-	useEffect(() => {
-		const {
-			client_id,
-			nonce,
-			response_type,
-			ready,
-			redirect_uri,
-			scope,
-			state,
-		} = router.query as Record<string, string>;
+  useEffect(() => {
+    const {
+      client_id,
+      nonce,
+      response_type,
+      ready,
+      redirect_uri,
+      scope,
+      state,
+    } = router.query as Record<string, string>;
 
-		if (!router.isReady) {
-			return;
-		}
+    if (!router.isReady) {
+      return;
+    }
 
-		if (!ready || !client_id) {
-			const urlParams = new URLSearchParams({
-				code: "invalid_request",
-				detail:
-					"Please call the /authorize endpoint with the required parameters.",
-				...router.query,
-			});
-			router.push(`/error?${urlParams}`);
-		}
+    if (!ready || !client_id) {
+      const urlParams = new URLSearchParams({
+        code: "invalid_request",
+        detail:
+          "Please call the /authorize endpoint with the required parameters.",
+        ...router.query,
+      });
+      router.push(`/error?${urlParams}`);
+    }
 
-		setParams({ client_id, nonce, response_type, redirect_uri, scope, state });
-	}, [router]);
+    setParams({ client_id, nonce, response_type, redirect_uri, scope, state });
+  }, [router]);
 
-	const handleIDKitSuccess = async (result: ISuccessResult) => {
-		const url = new URL("/api/authenticate", window.location.origin);
-		url.search = new URLSearchParams({ ...params, ...result }).toString();
-		window.location.href = url.toString();
-	};
+  const handleIDKitSuccess = async (result: ISuccessResult) => {
+    const url = new URL("/api/authenticate", window.location.origin);
+    url.search = new URLSearchParams({ ...params, ...result }).toString();
+    window.location.href = url.toString();
+  };
 
-	return (
-		<div className="flex justify-center items-center h-full">
-			<div>
-				<div className="bg-white py-8 px-12 rounded-xl text-center max-w-sm">
-					{headerShown && (
-						<>
-							<div className="flex justify-center">
-								<IconWorldcoin className="text-2xl" />
-							</div>
-							<h1 className="font-medium text-xl mt-6">
-								Sign in with Worldcoin
-							</h1>
-							<div className="text-text-muted mt-2">
-								Scan with the app to continue
-							</div>
-						</>
-					)}
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div>
+        <div className="bg-white py-8 px-12 rounded-xl text-center max-w-sm">
+          {headerShown && (
+            <>
+              <div className="flex justify-center">
+                <IconWorldcoin className="text-2xl" />
+              </div>
+              <h1 className="font-medium text-xl mt-6">
+                Sign in with Worldcoin
+              </h1>
+              <div className="text-text-muted mt-2">
+                Scan with the app to continue
+              </div>
+            </>
+          )}
 
-					{!params && <Spinner />}
+          {!params && <Spinner />}
 
-					{params && (
-						<IDKitBridge
-							client_id={params.client_id}
-							nonce={params.nonce}
-							toggleHeader={(visible) => setHeaderShown(visible)}
-							onSuccess={handleIDKitSuccess}
-						/>
-					)}
-				</div>
-				<div className="text-center text-gray-400 mt-2">or</div>
-				<a
-					href="https://worldcoin.org/download"
-					rel="noreferrer noopener"
-					target="_blank"
-				>
-					<div className="bg-white rounded-lg mt-2 px-4 py-3 flex items-center">
-						<div className="bg-text rounded p-1 mr-2">
-							<IconWorldcoin className="text-white text-xs" />
-						</div>
-						<div className="flex-grow">Sign up in the app</div>
-						<IconArrowRight className="text-2xl text-gray-400" />
-					</div>
-				</a>
-				<Footer />
-			</div>
-		</div>
-	);
+          {params && (
+            <IDKitBridge
+              client_id={params.client_id}
+              nonce={params.nonce}
+              toggleHeader={(visible) => setHeaderShown(visible)}
+              onSuccess={handleIDKitSuccess}
+            />
+          )}
+        </div>
+        <div className="text-center text-gray-400 mt-2">or</div>
+        <a
+          href="https://worldcoin.org/download"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <div className="bg-white rounded-lg mt-2 px-4 py-3 flex items-center">
+            <div className="bg-text rounded p-1 mr-2">
+              <IconWorldcoin className="text-white text-xs" />
+            </div>
+            <div className="flex-grow">Sign up in the app</div>
+            <IconArrowRight className="text-2xl text-gray-400" />
+          </div>
+        </a>
+        <Footer />
+      </div>
+    </div>
+  );
 }
