3ab7f3aaeeb1cbc58ece1835bdbbf0e254249519
Max Petretta <git@maxpetretta.com>
2023-02-27 22:32:52 -0800

    feat: desktop design changes (#6)

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -2,7 +2,7 @@ export const Spinner = () => {
   return (
     <div className="flex items-center justify-center">
       <div
-        className="border-gray-400 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        className="border-gray-400 inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
