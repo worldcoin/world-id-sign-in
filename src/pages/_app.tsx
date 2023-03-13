07d1b81a765e438a7dcb93dfea1d8197312f8e5d
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-03-13 15:57:47 +0100

    refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import posthog from "posthog-js";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
