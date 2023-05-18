806eef3d8454ff3df15b9c343a4a50e0338688b2
Miguel Piedrafita <github@miguel.build>
2023-05-18 19:02:52 +0200

    Allow POSTing to /authenticate

--- src/app/authenticate/route.ts
+++ src/app/authenticate/route.ts
@@ -102,3 +102,84 @@ export const GET = async (req: NextRequest): Promise<NextResponse> => {
 
   return NextResponse.redirect(url);
 };
+
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  const args = await req.json();
+
+  for (const attr of params) {
+    if (!args[attr]) return errorRequiredAttribute(attr);
+  }
+
+  const {
+    response_type,
+    client_id,
+    redirect_uri,
+    nonce,
+    merkle_root,
+    proof,
+    credential_type,
+    nullifier_hash,
+    state,
+    scope,
+  } = args;
+
+  const response = await fetch(`${DEVELOPER_PORTAL}/api/v1/oidc/authorize`, {
+    method: "POST",
+    headers: new Headers({ "content-type": "application/json" }),
+    body: JSON.stringify({
+      response_type,
+      app_id: client_id,
+      redirect_uri,
+      nonce,
+      merkle_root,
+      proof,
+      credential_type,
+      nullifier_hash,
+      scope,
+    }),
+  });
+
+  if (!response.ok) {
+    let errorResponse;
+    try {
+      errorResponse = await response.json();
+    } catch {
+      errorResponse = await response.text();
+    }
+
+    console.error(
+      `Could not authenticate OIDC user: ${response.statusText}`,
+      errorResponse
+    );
+
+    const detail = Object.hasOwn(errorResponse, "detail")
+      ? errorResponse.detail
+      : "We could not complete your authentication. Please try again.";
+
+    return NextResponse.json(
+      {
+        code: "authentication_failed",
+        detail,
+        response_type,
+        client_id,
+        redirect_uri,
+        state,
+        nonce,
+      },
+      { status: 400 }
+    );
+  }
+
+  const responseAuth = await response.json();
+
+  const url = new URL(redirect_uri!.toString());
+  if (responseAuth.code) url.searchParams.append("code", responseAuth.code);
+  if (responseAuth.token) url.searchParams.append("token", responseAuth.token);
+  if (responseAuth.id_token)
+    url.searchParams.append("id_token", responseAuth.id_token);
+
+  // FIXME: pass `state` in a secure cookie (signed) from original request to prevent tampering
+  if (state) url.searchParams.append("state", state.toString());
+
+  return NextResponse.json({ url });
+};
