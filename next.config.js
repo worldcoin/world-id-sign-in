33b753d75cae7b9de1e6197dab4ed7056125ad82
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-06-13 19:01:53 +0200

    fix: attempt to fix Vercel build

--- next.config.js
+++ next.config.js
@@ -3,24 +3,33 @@ const OIDC_ROUTES = ["/token", "/userinfo", "/register", "/introspect"];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	async rewrites() {
-		return [
-			...OIDC_ROUTES.map((route) => ({
-				source: route,
-				destination: "/oidc-route",
-			})),
-		];
-	},
-	async redirects() {
-		return [
-			{
-				source: "/",
-				permanent: false,
-				destination: "https://docs.worldcoin.org", // move to World ID docs
-			},
-		];
-	},
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        "utf-8-validate": "utf-8-validate",
+        bufferutil: "bufferutil",
+      });
+    }
+    return config;
+  },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      ...OIDC_ROUTES.map((route) => ({
+        source: route,
+        destination: "/oidc-route",
+      })),
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        permanent: false,
+        destination: "https://docs.worldcoin.org", // move to World ID docs
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
