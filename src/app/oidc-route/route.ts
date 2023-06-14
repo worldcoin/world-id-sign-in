8785b8f17da621c93f8d9e8b62c4fcfe5d8a3ddf
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-06-14 10:20:45 +0200

    all fixes

--- src/app/oidc-route/route.ts
+++ src/app/oidc-route/route.ts
@@ -8,25 +8,27 @@ const SPECIAL_MAPPING: Record<string, string> = {
 };
 
 /// Routes OIDC requests to the Developer Portal
-export const GET = async (req: NextRequest): Promise<NextResponse> => {
-  if (!req.url || req.url === "/oidc-route") {
-    return new NextResponse("Not found", { status: 404 });
+const handler = async (req: NextRequest): Promise<NextResponse> => {
+  const path = req.nextUrl.pathname;
+  if (!path || path === "/oidc-route") {
+    return NextResponse.json({ code: "not_found" }, { status: 404 });
   }
 
   const destUrl = new URL(
-    req.url in SPECIAL_MAPPING
-      ? `${DEVELOPER_PORTAL}${SPECIAL_MAPPING[req.url]}`
-      : `${DEVELOPER_PORTAL}/api/v1/oidc${req.url}`
+    path in SPECIAL_MAPPING
+      ? `${DEVELOPER_PORTAL}${SPECIAL_MAPPING[path]}`
+      : `${DEVELOPER_PORTAL}/api/v1/oidc${path}`
   );
 
   destUrl.search = req.nextUrl.searchParams.toString();
 
-  let headers = new Headers({
+  const headers = new Headers({
     "Content-Type": req.headers.get("content-type") || "application/json",
   });
 
-  if (req.headers.has("authorization"))
+  if (req.headers.has("authorization")) {
     headers.append("Authorization", req.headers.get("authorization")!);
+  }
 
   const body =
     req.headers.get("content-type") === "application/x-www-form-urlencoded"
@@ -39,8 +41,11 @@ export const GET = async (req: NextRequest): Promise<NextResponse> => {
     body: req.method === "POST" ? body : undefined,
   });
 
-  if (response.status === 404)
-    return new NextResponse("Not found", { status: 404 });
+  console.log(req.method, destUrl, response.status);
+
+  if (response.status === 404) {
+    return NextResponse.json({ code: "not_found" }, { status: 404 });
+  }
 
   if (response.status >= 500) {
     console.error(
@@ -59,18 +64,29 @@ export const GET = async (req: NextRequest): Promise<NextResponse> => {
     );
   }
 
+  const responseHeaders = new Headers();
+
+  // ANCHOR: Passthrough CORS headers
   const corsOrigin = response.headers.get("allow-control-allow-origin");
   const corsMethods = response.headers.get("allow-control-allow-methods");
   const corsHeaders = response.headers.get("allow-control-allow-headers");
 
-  headers = new Headers();
+  if (corsOrigin) {
+    responseHeaders.append("Access-Control-Allow-Origin", corsOrigin);
+  }
+  if (corsMethods) {
+    responseHeaders.append("Access-Control-Allow-Methods", corsMethods);
+  }
 
-  if (corsOrigin) headers.append("Access-Control-Allow-Origin", corsOrigin);
-  if (corsMethods) headers.append("Access-Control-Allow-Methods", corsMethods);
-  if (corsHeaders) headers.append("Access-Control-Allow-Headers", corsHeaders);
+  if (corsHeaders) {
+    responseHeaders.append("Access-Control-Allow-Headers", corsHeaders);
+  }
 
   return NextResponse.json(await response.json(), {
-    headers,
+    headers: responseHeaders,
     status: response.status,
   });
 };
+
+export const GET = handler;
+export const POST = handler;
