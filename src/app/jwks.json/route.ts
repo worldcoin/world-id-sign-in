7e4c0ac12bdae3c52eb471f7aafc0a6caab476ec
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-06-14 10:23:17 +0200

    fix: attempt to fix Vercel build (#15)

--- src/app/jwks.json/route.ts
+++
@@ -1,5 +0,0 @@
-import { DEVELOPER_PORTAL } from "@/consts";
-
-export const GET = async (): Promise<Response> => {
-	return fetch(`${DEVELOPER_PORTAL}/api/v1/jwks`);
-};
--- src/app/oidc-route/route.ts
+++ src/app/oidc-route/route.ts
@@ -2,70 +2,90 @@ import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { DEVELOPER_PORTAL } from "@/consts";
 
+const SPECIAL_MAPPING: Record<string, string> = {
+  "/jwks.json": "/api/v1/jwks",
+  "/.well-known/openid-configuration": "/api/v1/oidc/openid-configuration",
+};
+
+/// Routes OIDC requests to the Developer Portal
 const handler = async (req: NextRequest): Promise<NextResponse> => {
-	if (!req.url || req.url === "/oidc-route") {
-		return new NextResponse("Not found", { status: 404 });
-	}
+  const path = req.nextUrl.pathname;
+  if (!path || path === "/oidc-route") {
+    return NextResponse.json({ code: "not_found" }, { status: 404 });
+  }
 
-	const destUrl = new URL(
-		`${DEVELOPER_PORTAL}/api/v1/oidc${req.nextUrl.pathname}`
-	);
+  const destUrl = new URL(
+    path in SPECIAL_MAPPING
+      ? `${DEVELOPER_PORTAL}${SPECIAL_MAPPING[path]}`
+      : `${DEVELOPER_PORTAL}/api/v1/oidc${path}`
+  );
 
-	destUrl.search = req.nextUrl.searchParams.toString();
+  destUrl.search = req.nextUrl.searchParams.toString();
 
-	let headers = new Headers({
-		"Content-Type": req.headers.get("content-type") || "application/json",
-	});
+  const headers = new Headers({
+    "Content-Type": req.headers.get("content-type") || "application/json",
+  });
 
-	if (req.headers.has("authorization")) {
-		headers.append("Authorization", req.headers.get("authorization")!);
-	}
+  if (req.headers.has("authorization")) {
+    headers.append("Authorization", req.headers.get("authorization")!);
+  }
 
-	const body =
-		req.headers.get("content-type") === "application/x-www-form-urlencoded"
-			? new URLSearchParams(await req.json())
-			: JSON.stringify(req.body);
+  const body =
+    req.headers.get("content-type") === "application/x-www-form-urlencoded"
+      ? new URLSearchParams(await req.json())
+      : JSON.stringify(req.body);
 
-	const response = await fetch(destUrl, {
-		headers,
-		method: req.method,
-		body: req.method === "POST" ? body : undefined,
-	});
+  const response = await fetch(destUrl, {
+    headers,
+    method: req.method,
+    body: req.method === "POST" ? body : undefined,
+  });
 
-	if (response.status === 404) {
-		return new NextResponse("Not found", { status: 404 });
-	}
+  console.log(req.method, destUrl, response.status);
 
-	if (response.status >= 500) {
-		console.error(
-			`Received 500+ response from Developer Portal`,
-			req.url,
-			response.status
-		);
-		console.error(await response.text());
+  if (response.status === 404) {
+    return NextResponse.json({ code: "not_found" }, { status: 404 });
+  }
 
-		return NextResponse.json(
-			{
-				code: "server_error",
-				message: "Internal server error. Please try again.",
-			},
-			{ status: 500 }
-		);
-	}
+  if (response.status >= 500) {
+    console.error(
+      `Received 500+ response from Developer Portal`,
+      req.url,
+      response.status
+    );
+    console.error(await response.text());
 
-	const corsOrigin = response.headers.get("allow-control-allow-origin");
-	const corsMethods = response.headers.get("allow-control-allow-methods");
-	const corsHeaders = response.headers.get("allow-control-allow-headers");
+    return NextResponse.json(
+      {
+        code: "server_error",
+        message: "Internal server error. Please try again.",
+      },
+      { status: 500 }
+    );
+  }
 
-	headers = new Headers();
-	if (corsOrigin) headers.append("Access-Control-Allow-Origin", corsOrigin);
-	if (corsMethods) headers.append("Access-Control-Allow-Methods", corsMethods);
-	if (corsHeaders) headers.append("Access-Control-Allow-Headers", corsHeaders);
+  const responseHeaders = new Headers();
 
-	return NextResponse.json(await response.json(), {
-		headers,
-		status: response.status,
-	});
+  // ANCHOR: Passthrough CORS headers
+  const corsOrigin = response.headers.get("allow-control-allow-origin");
+  const corsMethods = response.headers.get("allow-control-allow-methods");
+  const corsHeaders = response.headers.get("allow-control-allow-headers");
+
+  if (corsOrigin) {
+    responseHeaders.append("Access-Control-Allow-Origin", corsOrigin);
+  }
+  if (corsMethods) {
+    responseHeaders.append("Access-Control-Allow-Methods", corsMethods);
+  }
+
+  if (corsHeaders) {
+    responseHeaders.append("Access-Control-Allow-Headers", corsHeaders);
+  }
+
+  return NextResponse.json(await response.json(), {
+    headers: responseHeaders,
+    status: response.status,
+  });
 };
 
 export const GET = handler;
