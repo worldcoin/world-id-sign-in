fb0fbb8280b9c946eff50940a3cbf56d566c810c
0xPenryn <penryn@pm.me>
2023-10-03 13:07:59 -0500

    feat: click-to-copy QR code

--- src/components/IDKitBridge.tsx
+++ src/components/IDKitBridge.tsx
@@ -1,11 +1,13 @@
 import { Spinner } from "./Spinner";
-import { memo, useEffect } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import { VerificationState } from "@worldcoin/idkit/build/src/types/app";
 import {
   ISuccessResult,
   CredentialType,
   internal as IDKitInternal,
 } from "@worldcoin/idkit";
+import copy from "copy-to-clipboard";
+import { AnimatePresence, LazyMotion, m, domAnimation } from "framer-motion";
 
 interface IIDKitBridge {
   nonce: string;
@@ -60,62 +62,93 @@ const IDKitBridge = ({
     if (qrData?.mobile) setDeeplink(qrData.mobile);
   }, [qrData, setDeeplink]);
 
+
+  const [copiedLink, setCopiedLink] = useState(false)
+  const copyLink = useCallback(() => {
+    copy(qrData?.default ?? '')
+
+    setCopiedLink(true)
+    setTimeout(() => setCopiedLink(false), 2000)
+  }, [qrData])
+
   return (
     <div className="md:mt-8">
       {verificationState ===
         IDKitInternal.VerificationState.AwaitingConnection && (
-        <>
-          {!qrData?.default && !qrData?.mobile && <Spinner />}
-          {qrData?.default && (
-            <>
-              {/* .qr-code className used for remote synthetic tests */}
-              <div className="hidden md:block qr-code">
-                <IDKitInternal.QRCode data={qrData?.default} size={280} />
-              </div>
-              <div className="md:hidden mt-10 md:mt-0">
-                <Spinner />
-                <div className="text-text-muted pt-4">
-                  Wait a few seconds, automatically opening World App
+          <>
+            {!qrData?.default && !qrData?.mobile && <Spinner />}
+            {qrData?.default && (
+              <>
+                {/* .qr-code className used for remote synthetic tests */}
+                <LazyMotion features={async () => (await import('./animations')).default}> {/* only load framer if displaying QR code */}
+                  <AnimatePresence>
+                    {copiedLink && (
+                      <m.div
+                        className="text-sm"
+                        key="copied"
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        variants={{
+                          visible: { opacity: 1, height: 'auto', marginBottom: 16, transition: { opacity: { duration: 0.25, delay: 0.1 }, height: { duration: 0.25 }, marginBottom: { duration: 0.25 }, ease: 'easeInOut' } },
+                          hidden: { opacity: 0, height: 0, marginBottom: 0 },
+                          exit: { opacity: 0, height: 0, marginBottom: 0, transition: { opacity: { duration: 0.25 }, height: { delay: 0.2, duration: 0.25 }, marginBottom: { delay: 0.2, duration: 0.25 }, ease: "easeInOut" } },
+                        }}
+                      >
+                        <span className="border-f1f5f8 rounded-lg border py-1 px-2 text-sm">
+                          {('QR Code copied')}
+                        </span>
+                      </m.div>
+                    )}
+                  </AnimatePresence>
+                </LazyMotion>
+                <div className="hidden md:block qr-code" onClick={copyLink}>
+                  <IDKitInternal.QRCode data={qrData?.default} size={280} />
                 </div>
-              </div>
-            </>
-          )}
-        </>
-      )}
+                <div className="md:hidden mt-10 md:mt-0">
+                  <Spinner />
+                  <div className="text-text-muted pt-4">
+                    Wait a few seconds, automatically opening World App
+                  </div>
+                </div>
+              </>
+            )}
+          </>
+        )}
       {verificationState !==
         IDKitInternal.VerificationState.AwaitingConnection && (
-        <>
-          <Spinner />
-          {verificationState ===
-            IDKitInternal.VerificationState.LoadingWidget && (
-            <>
-              <h1 className="font-medium text-3xl mt-12">Loading...</h1>
-              <div className="text-text-muted text-xl mt-2">
-                Please wait a moment
-              </div>
-            </>
-          )}
-          {verificationState ===
-            IDKitInternal.VerificationState.AwaitingVerification && (
-            <>
-              <h1 className="font-medium text-3xl mt-12">
-                Confirm in World App
-              </h1>
-              <div className="text-text-muted text-xl mt-2">
-                Waiting for your response
-              </div>
-            </>
-          )}
-          {verificationState === IDKitInternal.VerificationState.Confirmed && (
-            <>
-              <h1 className="font-medium text-3xl mt-12">Identity Confirmed</h1>
-              <div className="text-text-muted text-xl mt-2">
-                Taking you back to your app
-              </div>
-            </>
-          )}
-        </>
-      )}
+          <>
+            <Spinner />
+            {verificationState ===
+              IDKitInternal.VerificationState.LoadingWidget && (
+                <>
+                  <h1 className="font-medium text-3xl mt-12">Loading...</h1>
+                  <div className="text-text-muted text-xl mt-2">
+                    Please wait a moment
+                  </div>
+                </>
+              )}
+            {verificationState ===
+              IDKitInternal.VerificationState.AwaitingVerification && (
+                <>
+                  <h1 className="font-medium text-3xl mt-12">
+                    Confirm in World App
+                  </h1>
+                  <div className="text-text-muted text-xl mt-2">
+                    Waiting for your response
+                  </div>
+                </>
+              )}
+            {verificationState === IDKitInternal.VerificationState.Confirmed && (
+              <>
+                <h1 className="font-medium text-3xl mt-12">Identity Confirmed</h1>
+                <div className="text-text-muted text-xl mt-2">
+                  Taking you back to your app
+                </div>
+              </>
+            )}
+          </>
+        )}
     </div>
   );
 };
