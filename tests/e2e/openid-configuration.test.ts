50977477efb537c4c97f0f02c0d68011f476f9d4
Paolo <149602456+pdtfh@users.noreply.github.com>
2024-01-09 11:24:47 -0600

    add test case

---
+++ tests/e2e/openid-configuration.test.ts
@@ -0,0 +1,25 @@
+import { GET } from "@/app/.well-known/openid-configuration/route";
+import { NextRequest } from "next/server";
+
+describe("/.well-known/openid-configuration", () => {
+  test("can fetch params", async () => {
+    const req = new NextRequest("http://localhost/authenticate", {
+      method: "GET",
+    });
+    const response = await GET(req);
+    expect(response.status).toBe(200);
+
+    const body = await response.json();
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        issuer: "https://id.worldcoin.org",
+        token_endpoint: "https://id.worldcoin.org/token",
+        jwks_uri: "https://id.worldcoin.org/jwks.json",
+        code_challenge_methods_supported: ["S256"],
+        subject_types_supported: ["pairwise"],
+        response_modes_supported: ["query", "fragment", "form_post"],
+      })
+    );
+  });
+});
