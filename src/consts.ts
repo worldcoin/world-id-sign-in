e245ca5fb2cf6cc160bf152572061768e7902c44
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-05-10 12:49:22 -0300

    update-const (#75)

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,14 @@
 export const DEVELOPER_PORTAL =
-  process.env.NEXT_PUBLIC_DEVELOPER_PORTAL ?? "https://developer.worldcoin.org";
+  process.env.NEXT_PUBLIC_DEVELOPER_PORTAL ??
+  process.env.NEXT_PUBLIC_ENV === "staging"
+    ? "https://staging.developer.worldcoin.org"
+    : "https://developer.worldcoin.org";
 
 // ANCHOR: OIDC Base URL
 export const OIDC_BASE_URL =
-  process.env.NEXT_PUBLIC_URL ?? "https://id.worldcoin.org";
+  process.env.NEXT_PUBLIC_URL ?? process.env.NEXT_PUBLIC_ENV === "staging"
+    ? "https://staging.id.worldcoin.org"
+    : "https://id.worldcoin.org";
 
 // ANCHOR: JWT Issuer
 export const JWT_ISSUER =
