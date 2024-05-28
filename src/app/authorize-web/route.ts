a2eb1117a44a647c82b5eeaf612549e0d2063d76
0xPenryn <ian.dilick@toolsforhumanity.com>
2024-05-28 10:19:36 -0700

    fix: use public url for redirect

--- src/app/authorize-web/route.ts
+++ src/app/authorize-web/route.ts
@@ -2,8 +2,8 @@ import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest): Promise<NextResponse> => {
-  let url = new URL(req.url);
-  url.pathname = "/authorize";
+  let url = new URL("/authorize", process.env.NEXT_PUBLIC_URL);
+  url.search = new URL(req.url).searchParams.toString();
 
   let res = NextResponse.redirect(url, {
     status: 303,
