73ca0691160f432f318391edaf0334dc1adf2daa
0xPenryn <ian.dilick@toolsforhumanity.com>
2024-06-05 18:05:37 +0200

    feat: authorize-web re-exports authorize

--- src/app/authorize-web/route.ts
+++ src/app/authorize-web/route.ts
@@ -1,14 +1,2 @@
-import { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-export const GET = async (req: NextRequest): Promise<NextResponse> => {
-  let url = new URL("/authorize", process.env.NEXT_PUBLIC_URL);
-  url.search = new URL(req.url).searchParams.toString();
-
-  let res = NextResponse.redirect(url, {
-    status: 303,
-    headers: { "Set-Cookie": `web-only=true` },
-  });
-
-  return res;
-};
+// only purpose here is to avoid deeplinking to World App if this url is used
+export { GET } from "../authorize/route";
