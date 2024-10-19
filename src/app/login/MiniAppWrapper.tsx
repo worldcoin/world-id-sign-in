e3d47e1ed9478080b7dca24edf289f2ba312ab34
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-18 18:21:08 -0700

    fix redirects

--- src/app/login/MiniAppWrapper.tsx
+++ src/app/login/MiniAppWrapper.tsx
@@ -15,23 +15,44 @@ type MiniAppRouterProps = {
   code_challenge_method?: string;
 };
 
-export const MiniAppRouter = ({
-  client_id,
-  nonce,
-  response_type,
-  ready,
-  redirect_uri,
-  scope,
-  state,
-  response_mode,
-  code_challenge,
-  code_challenge_method,
-}: MiniAppRouterProps) => {
+export const MiniAppRouter = (props: MiniAppRouterProps) => {
+  const {
+    client_id,
+    nonce,
+    response_type,
+    ready,
+    redirect_uri,
+    scope,
+    state,
+    response_mode,
+    code_challenge,
+    code_challenge_method,
+  } = props;
   const router = useRouter();
+
   // If window.WorldApp is defined redirect to /authorize again with the params
+  function buildAuthorizeUrl(params: any): string {
+    const queryParams = Object.entries(params)
+      .filter(([_, value]) => value !== undefined)
+      .map(([key, value]) => `${key}=${encodeURIComponent(value as string)}`)
+      .join("&");
+
+    return `/authorize?${queryParams}`;
+  }
+
   if (window.WorldApp) {
     router.replace(
-      `/authorize?response_type=${response_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}&nonce=${nonce}&response_mode=${response_mode}&code_challenge=${code_challenge}&code_challenge_method=${code_challenge_method}`
+      buildAuthorizeUrl({
+        response_type,
+        client_id,
+        redirect_uri,
+        scope,
+        state,
+        nonce,
+        response_mode,
+        code_challenge,
+        code_challenge_method,
+      })
     );
   }
   return <></>;
